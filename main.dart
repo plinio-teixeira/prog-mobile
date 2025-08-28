e5719fc51012b588937465e17bed8f482ac03b73
plinio-teixeira <plinio.t.vasconcellos@uni9.edu.br>
2025-08-28 01:03:53 +0000

    finsih

--- main.dart
+++ main.dart
@@ -1,4 +1,24 @@
+import 'dart:io';
+
 void main(){
-    String nome = "José";
-    print("olá, $nome");
-}
+
+    List<String?> alunos = [];
+    bool cadastroOutroAluno = true;
+
+    while(cadastroOutroAluno){
+        print("Informe o nome do aluno:");
+        String? nome = stdin.readLineSync();
+
+        alunos.add(nome);
+
+        print('Deseja cadastrar outros alunos?(S / N)');
+        var respostaUsuario = stdin.readLineSync();
+        if(respostaUsuario == "S"){
+            cadastroOutroAluno = true;
+        }
+        else{
+            cadastroOutroAluno = false;
+        }
+    }
+ } //      (segunda forma) cadastroOutroAluno = respostaUsuario == "S";
+    
