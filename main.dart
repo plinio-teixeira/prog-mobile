7629c9bd81f786f79173b9bb535d96454924d98b
plinio-teixeira <plinio.t.vasconcellos@uni9.edu.br>
2025-09-04 00:56:53 +0000

    feat:aula3/comando-else-if

--- main.dart
+++ main.dart
@@ -44,21 +44,22 @@ void main(){
     } */
     import 'dart:io';
     void main (){
-        int renda = 1400;
-        if ( renda >1400 )
-        {
-            print('varejo');
+        int renda = 1000000;
+        int saldo = 5000000;
+        if ( renda > 1400 && renda < 5000 )
+        {print("varejo");
+        
         }
-        else if (renda > 5000)
-        {
-            print('vangogh')
-        }
-        else if (renda > 30000)
-        {
-            print('select')
-        }
-        else (renda >= 60000)
-        {
-            print('exclusive')
-        }
+
+        else if (renda > 5000 && renda < 30000)
+        {print("vangogh");}
+
+        else if (renda > 30000 && renda < 60000)
+           {print("select");}
+
+        else if(renda >= 60000 && renda < 100000)
+        {print("exclusive");}
+
+         else if(renda >= 60000 && saldo > 100000)
+        {print("exclusive");}
     }
