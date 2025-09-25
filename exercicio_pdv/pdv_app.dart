7cdf350e489f15450d837121a819c06d71805c27
plinio-teixeira <plinio.t.vasconcellos@uni9.edu.br>
2025-09-25 00:10:29 +0000

    feat:PDV_app

---
+++ exercicio_pdv/pdv_app.dart
@@ -0,0 +1,54 @@
+import 'dart.io';
+import 'produto.dart';
+import 'item_compra.dart';
+import 'carrinho_compra.dart';
+
+class PDVApp {
+  CarrinhoCompra carrinho = CarrinhoCompra();
+
+  void iniciar() {
+    print('\n== SISTEMA DE PDV\N');
+    print('Bem-vindo ao sistema!');
+
+    bool continuar = true;
+  
+        while (continuar) {
+        print('Deseja adicionar um produto ao carrinho? (S/N)');
+            String resposta = stdin.readLineSync()?.toUpperCase() ?? 'N'
+        
+            if (resposta == 'S'){
+                ItemCompra item = lerDadosCompra();
+                carrinho.adicionarItem(item);
+                print('Produto adicionado ao carrinho!\n');
+            } else(resposta == 'N'){
+                continuar = false;
+            }
+        }
+
+        if (carrinho.estaVazio()){
+            print('\nCarrinho vazio. Nenhum produto foi adicionado.');
+            print('\nObrigado Por usar o sistema PDV.');
+        } else {
+            exibirResumoCompra();
+        }
+        ItemCompra lerDadosCompra() {
+            print('\nInforme o nome do produto--');
+            print('\nNome do proto');
+            String nome = stdin.readLineSync() ?? '';
+
+            double valor = 0;
+            bool valorValido = false;
+
+            while(!valorValido){
+                print('\nvalor unitario');
+                String input = stdin.readLineSync() ?? '0';
+                valorUnitario = double.parse(input.replaceAll(',', '.'));
+                if(valorUnitario <= 0){
+                    print('\nValor desse ser maior que zero.');
+                } else {
+                    valorValido = true;
+                }
+
+            }
+    } 
+}
